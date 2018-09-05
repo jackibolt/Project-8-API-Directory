815066fc60ba919738bae65f7602e75ce99add44
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-09-04 21:27:17 -0400

    going in circles

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@
 console.log('it works');
 
 const grid = document.querySelector('#grid');
-const card = document.querySelectorAll('.card');
+const card = document.getElementsByClassName('card');
 const overlay = document.querySelector('#overlay');
 
 //// HELPER FUNCTIONS
@@ -14,6 +14,7 @@ const createChildElement = (tagName, className, innerHTML, parentElement) => {
   parentElement.append(addElement);
 };
 
+
 // Fetch
 fetch('https://randomuser.me/api/?results=12&?nat=us')
   .then(response => response.json())
@@ -22,36 +23,37 @@ fetch('https://randomuser.me/api/?results=12&?nat=us')
     const employeeData = data.results;
 
     for(i=0; i<employeeData.length; i+=1){
-      const fullName = `${employeeData[i].name.first} ${employeeData[i].name.last}`;
       let employeeCard =
-          `<img src='${employeeData[i].picture.large}' alt='${fullName}' class='profile-pic'>
+          `<img src='${employeeData[i].picture.large}' alt='${employeeData[i].name.first} ${employeeData[i].name.last}' class='profile-pic'>
             <div class='text-info'>
-              <p class='name'>${fullName}</p>
+              <p class='name'>${employeeData[i].name.first} ${employeeData[i].name.last}</p>
               <p class='email'>${employeeData[i].email}</p>
               <p class='city'>${employeeData[i].location.city}</p>
             </div>`;
       createChildElement('div', 'card', employeeCard, grid);
+    }
 
-      const address = `${employeeData[i].location.street} ${employeeData[i].location.city} ${employeeData[i].location.state} ${employeeData[i].location.postcode}`;
-      const showEmployeeOverlay = (employee) => {
-        overlay.style.display = 'none';
-        let employeeOverlay =
-            `<img src="${employeeData[i].picture.large}" alt="${fullName}" class='overlay-pic'>
+    // create overlay
+    const showEmployeeOverlay = (data, i) => {
+      let employeeOverlay =
+          `<img src="${employeeData[i].picture.large}" alt="${employeeData[i].name.first} ${employeeData[i].name.last}" class='overlay-pic'>
             <div class='main-info'>
-              <h3>${fullName}</h3>
+              <h3>${employeeData[i].name.first} ${employeeData[i].name.last}</h3>
               <p>${employeeData[i].email}</p>
               <p>${employeeData[i].location.city}</p>
             </div>
             <div class='contact'>
               <p>${employeeData[i].cell}</p>
-              <p>${address}</p>
+              <p>${employeeData[i].location.street} ${employeeData[i].location.city} ${employeeData[i].location.state} ${employeeData[i].location.postcode}</p>
               <p>Birthday: ${employeeData[i].dob.date}</p>
             </div>`;
-        createChildElement('div', 'box-content', employeeOverlay, overlay);
-      }
-      grid.addEventListener('click', () => {
-        showEmployeeOverlay(employeeData[i]);
-        overlay.style.display = 'flex';
-      });
+      createChildElement('div', 'box-content', employeeOverlay, overlay);
+      overlay.style.display='flex';
     }
+
+    // click handler
+    grid.addEventListener('click', showEmployeeOverlay(employeeData, i));
+
 });
+
+console.log(card);
